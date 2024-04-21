1c1b49e41c3ab36143976c4de171884593c28a36
Akuro1411 <nahidisayev147@gmail.com>
2024-04-21 18:42:14 +0000

    code is designed for betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,32 @@
 #include "sort.h"
 
 /**
- * main - Entry point
+ * selection_sort - sorts the list with selection sort
+ * @array: - input of array
+ * @size: - input of size of array
  *
- * Return: Always 0
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp, change_num, n;
+	size_t i, j;
+	int tmp, change_num, n;
 
-for (i = 0; i < size; i++)
-{
-tmp = i;
-n = 0;
-for (j = i + 1; j < size; j++)
-{
-if (array[tmp] > array[j])
-{
-tmp = j;
-n += 1;
-}
-}
-change_num = array[i];
-array[i] = array[tmp];
-array[tmp] = change_num;
-if (n != 0)
-print_array(array, size);
-}
+	for (i = 0; i < size; i++)
+	{
+		tmp = i;
+		n = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[tmp] > array[j])
+			{
+				tmp = j;
+				n += 1;
+			}
+		}
+		change_num = array[i];
+		array[i] = array[tmp];
+		array[tmp] = change_num;
+		if (n != 0)
+		print_array(array, size);
+	}
 }
